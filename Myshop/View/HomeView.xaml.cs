245b993d056fd9811a6ca09a2ed74c985589ff4f
Tuanquan127 <ltquan20@clc.fitus.edu.vn>
2023-04-20 13:35:08 +0700

    add logic to show graph

--- Myshop/View/HomeView.xaml.cs
+++ Myshop/View/HomeView.xaml.cs
@@ -24,5 +24,10 @@ namespace Myshop.View
         {
             InitializeComponent();
         }
+
+        private void CartesianChart_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
--- Myshop/ViewModel/DashBoardViewModel.cs
+++ Myshop/ViewModel/DashBoardViewModel.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Drawing;
+using System.Net.Http;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Media;
@@ -16,14 +18,21 @@ namespace Myshop.ViewModel
     
     public class DashBoardViewModel: ViewModelBase
     {
-        private SeriesCollection profitCollection;
+        private enum ProfitType
+        {
+            date = 1,
+            month = 2,
+            year = 3,
+            week = 4
+        }
+        private SeriesCollection profitCollection = new();
         public SeriesCollection ProfitCollection { get { return profitCollection; } set {
                 profitCollection = value;
                 OnPropertyChanged(nameof(ProfitCollection));
             } }
 
 
-        private SeriesCollection topSoldCollection;
+        private SeriesCollection topSoldCollection = new();
         public SeriesCollection TopSoldCollection
         {
             get { return topSoldCollection; }
@@ -32,47 +41,293 @@ namespace Myshop.ViewModel
                 OnPropertyChanged(nameof(TopSoldCollection));
             }
         }
+
+        private CollectionViewSource RunningOutItemsSource = new CollectionViewSource { };
+        public ICollectionView RunningOutCollection => RunningOutItemsSource.View;
+
+        private int totalBooks;
+        public int TotalBooks { get; set; }
+
+        private int totalOrders;
+        public int TotalOrders { get; set; }
+
+        private decimal totalProfit;
+        public decimal TotalProfit { get; set; }
+
+        private string xTitle;
+        public string XTitle
+        {
+            get; set;
+        }
+
+        private List<string> xLabels;
+        public List<string> XLabels { get; set; }   
         public DashBoardViewModel()
         {
-            ProfitCollection = new SeriesCollection()
-            {
-                new LineSeries
-                {
-                    Values = new ChartValues<double>
-                    {
-                        3,4,5,6
-                    },
-                    Stroke= System.Windows.Media.Brushes.Blue,
-                    Fill = System.Windows.Media.Brushes.Red
-                },new ColumnSeries
-                {
-                    Values = new ChartValues<ObservableValue>
-                    {
-                       
-                    },
-                    Stroke= System.Windows.Media.Brushes.Orange,
-                    Fill = System.Windows.Media.Brushes.Gray,
-                    StrokeDashArray = new DoubleCollection{2}
-                }  
-            };
+            getProfit("", (int)ProfitType.date);
+            getNumBook();
+            getNumOrder();
+            getOutOfStock();
+        }
 
-            TopSoldCollection = new SeriesCollection
+        public async Task getProfit(string date, int typeProfit)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://hcmusshop.azurewebsites.net/api/Order");
+            using (var response = await client.SendAsync(request))
             {
-                new PieSeries
+                response.EnsureSuccessStatusCode();
+                var r = await response.Content.ReadAsStringAsync();
+                var json = JsonNode.Parse(r).AsArray();
+
+                List<decimal> orderTotal = new();
+                DateTime currentDate = DateTime.Today;
+                ChartValues<Decimal> decimals = new();
+                Dictionary<string, int> soldItems = new();
+                if (typeProfit == (int)ProfitType.date)
                 {
-                    Values = new ChartValues<decimal>{1, 3, 4, 5}
+                    for(int i = 0; i < json.Count; i++)
+                    {
+                        string dateString = json[i]["dateCreated"].ToString();
+                        DateTime orderCreatedDate = DateTime.ParseExact(dateString, "dd/MM/yyyy", null);
+                        if(DateTime.Compare(currentDate, orderCreatedDate) >= 0)
+                        {
+                            orderTotal.Add(decimal.Parse(json[i]["total"].ToString()));
+                            var jsonArray = json[i]["orderDetail"].AsArray();
+                            foreach (var item in jsonArray)
+                            {
+                                var amount = int.Parse(item["amount"].ToString());
+                                var bookString = item["book"].AsObject();
+                                BookInOrder temp = new BookInOrder(bookString, amount);
+                                if (soldItems.ContainsKey(temp.Title))
+                                {
+                                    soldItems[temp.Title] += amount;
+                                }
+                                else
+                                {
+                                    soldItems[temp.Title] = amount;
+                                }
+                            }
+                        }
+                    }
+
+                    foreach(var item in orderTotal)
+                    {
+                        decimals.Add(item);
+                    }
+
+                    foreach (var solditem in soldItems)
+                    {
+                        var key = solditem.Key;
+                        var value = solditem.Value;
+
+                        TopSoldCollection.Add(
+                            new PieSeries
+                            {
+                                Title = key,
+                                Values = new ChartValues<int>() { value}
+                            }
+                         );
+                    }
+
+
+                    ProfitCollection = new SeriesCollection {
+                                new ColumnSeries{
+                                    DataLabels = true,
+                                    Values = decimals,
+                                    Title = "Doanh thu theo ngày"
+                                }
+                            };
+
+                    OnPropertyChanged(nameof(ProfitCollection));
+
+
+                    XTitle = "Giờ";
+                    XLabels = new List<string>(){ "12:00", "13:01", "16:00" };
+                    
+                    OnPropertyChanged(nameof(XTitle));
+                    OnPropertyChanged(nameof(XLabels));
                 }
-            };
+                else if(typeProfit == (int)ProfitType.month)
+                {
+                    int currentMonth = currentDate.Month;
+                    Dictionary<int, decimal> dailyProfit = new();
+                    for (int i = 0; i < json.Count; i++)
+                    {
+                        string dateString = json[i]["dateCreated"].ToString();
+                        DateTime orderCreatedDate = DateTime.ParseExact(dateString, "dd/MM/yyyy", null);
+                        int month = orderCreatedDate.Month;
+                        if (month == currentMonth)
+                        {
+                            if (dailyProfit.ContainsKey(orderCreatedDate.Day))
+                            {
+                                dailyProfit[orderCreatedDate.Day] += decimal.Parse(json[i]["total"].ToString());
+                            }
+                            else
+                            {
+                                dailyProfit[orderCreatedDate.Day] = decimal.Parse(json[i]["total"].ToString());
+                            }  
+                        }
+                    }
+
+                    foreach (var item in dailyProfit.Values)
+                    {
+                        decimals.Add(item);
+                    }
+
+
+                    ProfitCollection = new SeriesCollection {
+                                new ColumnSeries{
+                                    Values = decimals,
+                                    Title = "Doanh thu theo tháng"
+                                }
+                            };
+
+                    OnPropertyChanged(nameof(ProfitCollection));
+
+                    List<string> labels = new();
+                    foreach(var day in dailyProfit.Keys)
+                    {
+                        labels.Add(day.ToString());
+                    }
+
+                    XTitle = "Ngày";
+                    XLabels = labels;
+
+                    OnPropertyChanged(nameof(XTitle));
+                    OnPropertyChanged(nameof(XLabels));
+                }
+                else if(typeProfit == (int)ProfitType.year)
+                {
+                    int currentYear = currentDate.Year;
+                    Dictionary<int, decimal> monthlyProfit = new();
+                    for (int i = 0; i < json.Count; i++)
+                    {
+                        string dateString = json[i]["dateCreated"].ToString();
+                        DateTime orderCreatedDate = DateTime.ParseExact(dateString, "dd/MM/yyyy", null);
+                        int year = orderCreatedDate.Year;
+                        if (year == currentYear)
+                        {
+                            if (monthlyProfit.ContainsKey(orderCreatedDate.Month))
+                            {
+                                monthlyProfit[orderCreatedDate.Month] += decimal.Parse(json[i]["total"].ToString());
+                            }
+                            else
+                            {
+                                monthlyProfit[orderCreatedDate.Month] = decimal.Parse(json[i]["total"].ToString());
+                            }
+                        }
+                    }
+
+                    foreach (var item in monthlyProfit.Values)
+                    {
+                        decimals.Add(item);
+                    }
+
+
+                    ProfitCollection = new SeriesCollection {
+                                new ColumnSeries{
+                                    Values = decimals,
+                                    Title = "Doanh thu theo năm"
+                                }
+                            };
+
+                    OnPropertyChanged(nameof(ProfitCollection));
+
+                    List<string> labels = new();
+                    foreach (var day in monthlyProfit.Keys)
+                    {
+                        labels.Add(day.ToString());
+                    }
+
+                    XTitle = "Tháng";
+                    XLabels = labels;
+
+                    OnPropertyChanged(nameof(XTitle));
+                    OnPropertyChanged(nameof(XLabels));
+                }       
+            }
         }
 
         public async Task getNumBook()
         {
- 
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://hcmusshop.azurewebsites.net/api/Book");
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var r = await response.Content.ReadAsStringAsync();
+                var json = JsonNode.Parse(r).AsArray();
+
+                TotalBooks = json.Count;
+                OnPropertyChanged(nameof(TotalBooks));
+            }
         }
 
         public async Task getNumOrder()
         {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://hcmusshop.azurewebsites.net/api/Order");
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var r = await response.Content.ReadAsStringAsync();
+                var json = JsonNode.Parse(r).AsArray();
 
+                TotalOrders = json.Count;
+                OnPropertyChanged(nameof(TotalOrders));
+
+                decimal tempProfit = 0;
+                for(int i = 0; i < TotalOrders; i++)
+                {
+                    var total = decimal.Parse(json[i]["total"].ToString());
+                    tempProfit += total;
+                }
+
+                TotalProfit = tempProfit;
+                OnPropertyChanged(nameof(TotalProfit));
+            }
+        }
+
+        public async Task getOutOfStock()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://hcmusshop.azurewebsites.net/api/Book");
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var r = await response.Content.ReadAsStringAsync();
+                var json = JsonNode.Parse(r).AsArray();
+
+                int count = 5;
+                List<Book> books = new();
+                for(int i = 0; i < json.Count; i++)
+                {
+                    if (count > 0)
+                    {
+                        var amount = int.Parse(json[i]["amount"].ToString());
+                        var title = json[i]["title"].ToString();
+
+                        if (amount <= 5)
+                        {
+                            books.Add(new
+                                Book
+                            { 
+                                title = title,
+                                Amount = amount
+                            }
+                            );
+                            count--;
+                        }
+                    }
+                    else
+                    {
+                        RunningOutItemsSource = new CollectionViewSource { Source = books };
+                        OnPropertyChanged(nameof(RunningOutCollection));
+                        return;
+                    }
+                }
+            }
         }
     }
 }
